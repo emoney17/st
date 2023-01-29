25d4c248b93b3fec2260bd94d10a94cb681815db
emoney17 <emmanuel.million@tutanota.com>
2023-01-28 23:01:42 -0600

    ...

--- config.def.h
+++ config.def.h
@@ -5,7 +5,6 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-// static char *font = "Iosevka Nerd Font:pixelsize=15:antialias=true:autohint=true";
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
